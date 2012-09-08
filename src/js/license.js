dc6e9c338d590bdf0d99565712455e7b6af7b8b8
Alexandru Chiculita <achicu@adobe.com>
2012-09-07 17:41:45 -0700

    updated license header

---
+++ src/js/license.js
@@ -0,0 +1,12 @@
+//
+// Copyright (c) 2002-2011 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// This file was automatically generated from the ANGLE project [1] using emscripten [2].
+//
+// [1] http://code.google.com/p/angleproject/
+// [2] https://github.com/kripken/emscripten
+//
+// For more information go to https://github.com/adobe/angle.js
+//
